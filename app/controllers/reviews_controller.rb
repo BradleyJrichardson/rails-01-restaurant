95689285d26e91a41e9754f00f9598bd7e2b5c3b
BradleyJrichardson <brdl.richardson@gmail.com>
2019-04-23 12:52:38 +1000

    added feature - need to log in to leave a review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -4,6 +4,13 @@ class ReviewsController < ApplicationController
   def create
     @restaurant = Restaurant.find(params[:restaurant_id])
     @review = @restaurant.reviews.create(review_params)
+
+    if @review.save
+      flash[:success] = 'review created'
+    else
+      flash[:error] = 'Log in to leave a review'
+    end
+
     redirect_to restaurant_path(@restaurant.id)
   end
 
